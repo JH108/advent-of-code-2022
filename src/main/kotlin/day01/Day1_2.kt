f8928b428004a5adeb3ead72e6216d22974a212b
Jesse Hill <jesse_hill@eogresources.com>
2022-12-02 09:19:04 -0500

    Remove the first two solutions that had already been pushed to the template repo

--- src/main/kotlin/day01/Day1_2.kt
+++ src/main/kotlin/day01/Day1_2.kt
@@ -3,9 +3,5 @@ package day01
 import solve
 
 fun main() = solve { lines ->
-    mutableListOf(0).also { elves ->
-        lines.forEach { line ->
-            if (line.isBlank()) elves += 0 else elves[elves.lastIndex] += line.toInt()
-        }
-    }.sorted().takeLast(3).sum()
+
 }
