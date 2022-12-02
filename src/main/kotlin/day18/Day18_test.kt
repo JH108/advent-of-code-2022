de7eee35d772a389aa5119ea15cbc1d8f6c6b79c
Jesse Hill <jesse_hill@eogresources.com>
2022-12-02 11:53:26 -0500

    Create all the test files

---
+++ src/main/kotlin/day18/Day18_test.kt
@@ -0,0 +1,22 @@
+import solve
+
+private fun part1Test(lines: List<String>): Int? {
+    TODO()
+}
+
+private fun part2Test(lines: List<String>): Int? {
+    TODO()
+}
+
+fun main() = solve { lines ->
+    val input = lines
+    val part1Expected = 1
+    val part2Expected = 1
+    val part1Actual = part1Test(input)
+    val part2Actual = part2Test(input)
+
+    assert(part1Expected == part1Actual)
+    assert(part2Expected == part2Actual)
+
+    true
+}
