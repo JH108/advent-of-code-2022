a8dca756d3a79660b37221ed5771a917d497e378
Jesse Hill <jesse_hill@eogresources.com>
2022-12-02 11:44:52 -0500

    Make solution for part 1 day 2

--- src/main/kotlin/day02/Day2_1.kt
+++ src/main/kotlin/day02/Day2_1.kt
@@ -2,6 +2,65 @@ package day02
 
 import solve
 
-fun main() = solve { lines ->
+fun String.convertToPlay(): RPCPlay = when (this) {
+    "A", "X" -> RPCPlay.Rock
+    "B", "Y" -> RPCPlay.Paper
+    "C", "Z" -> RPCPlay.Scissors
+    else -> throw Exception("Unknown Play")
+}
 
+data class Round(
+    val elf: RPCPlay,
+    val player: RPCPlay
+) {
+    private val winner = when {
+        elf == player -> RoundResult.Draw
+        elf greaterThan player -> RoundResult.ElfWin
+        else -> RoundResult.PlayerWin
+    }
+    val score = player.toScore() + winner.toScore()
+}
+
+enum class RoundResult {
+    Draw,
+    ElfWin,
+    PlayerWin
+}
+
+fun RoundResult.toScore() = when (this) {
+    RoundResult.Draw -> 3
+    RoundResult.ElfWin -> 0
+    RoundResult.PlayerWin -> 6
+}
+
+enum class RPCPlay {
+    Rock,
+    Paper,
+    Scissors
+}
+
+infix fun RPCPlay.greaterThan(other: RPCPlay) = when {
+    this == RPCPlay.Rock && other == RPCPlay.Scissors -> true
+    this == RPCPlay.Paper && other == RPCPlay.Rock -> true
+    this == RPCPlay.Scissors && other == RPCPlay.Paper -> true
+    else -> false
+}
+
+fun RPCPlay.toScore() = when (this) {
+    RPCPlay.Rock -> 1
+    RPCPlay.Paper -> 2
+    RPCPlay.Scissors -> 3
+}
+
+internal fun List<String>.toRounds() = map {
+    Round(
+        elf = it.substringBefore(" ").convertToPlay(),
+        player = it.substringAfter(" ").convertToPlay()
+    )
+}
+
+fun main() = solve { lines ->
+    val rounds = lines.toRounds()
+
+    rounds.sumOf { it.score }
 }
