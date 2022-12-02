43724950a7035f1524964a3caf8756b4a1c08901
Jesse Hill <jesse_hill@eogresources.com>
2022-12-02 10:40:04 -0500

    Write simple test for day 1

--- src/main/kotlin/day01/Day1_2.kt
+++ src/main/kotlin/day01/Day1_2.kt
@@ -2,29 +2,6 @@ package day01
 
 import solve
 
-fun attempt1(lines: List<String>) {
-    val elves = lines.buildElves()
-    var remainingElves = elves
-    var outputElves = listOf<CalorieElf>()
-
-    repeat(3) {
-        val bestFedElf = remainingElves.maxByOrNull { it.totalCalories }
-        bestFedElf?.run {
-            outputElves = outputElves + bestFedElf
-            remainingElves = remainingElves - bestFedElf
-        }
-    }
-
-    outputElves.sumOf { it.totalCalories }
-}
-
-fun attempt2(lines: List<String>) {
-    val elves = lines.buildElves()
-    val topThreeElves = elves.sortedByDescending { it.totalCalories }.take(3)
-
-    topThreeElves.sumOf { it.totalCalories }
-}
-
 fun main() = solve { lines ->
     val elves = lines.buildElves()
     val topThreeElves = elves.sortedByDescending { it.totalCalories }.take(3)
---
+++ src/main/kotlin/day01/Day1_test.kt
@@ -0,0 +1,28 @@
+package day01
+
+import solve
+
+fun part1Test(elves: List<CalorieElf>): Int? {
+    val bestFedElf = elves.maxByOrNull { it.totalCalories }
+g
+    return bestFedElf?.totalCalories
+}
+
+fun part2Test(elves: List<CalorieElf>): Int? {
+    val topThreeElves = elves.sortedByDescending { it.totalCalories }.take(3)
+
+    return topThreeElves.sumOf { it.totalCalories }
+}
+
+fun main() = solve { lines ->
+    val elves = lines.buildElves()
+    val part1Expected = 24000
+    val part2Expected = 45000
+    val part1Actual = part1Test(elves)
+    val part2Actual = part2Test(elves)
+
+    assert(part1Expected == part1Actual)
+    assert(part2Expected == part2Actual)
+
+    true
+}
