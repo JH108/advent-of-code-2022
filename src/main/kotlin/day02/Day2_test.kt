64aeb2bc9f32bd7b6d694942a1fd39457f1fe9b7
Jesse Hill <jesse_hill@eogresources.com>
2022-12-02 12:04:23 -0500

    Solve part 1 of day 2

--- src/main/kotlin/day02/Day2_test.kt
+++ src/main/kotlin/day02/Day2_test.kt
@@ -1,16 +1,20 @@
+package day02
+
 import solve
 
-private fun part1Test(lines: List<String>): Int? {
-    TODO()
+private fun part1Test(lines: List<String>): Int {
+    val rounds = lines.toRounds()
+
+   return rounds.sumOf { it.score }
 }
 
-private fun part2Test(lines: List<String>): Int? {
-    TODO()
+private fun part2Test(lines: List<String>): Int {
+    return 3
 }
 
 fun main() = solve { lines ->
     val input = lines
-    val part1Expected = 1
+    val part1Expected = 15
     val part2Expected = 1
     val part1Actual = part1Test(input)
     val part2Actual = part2Test(input)
